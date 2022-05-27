7c7c5863f4d7f80ad3c56efe91df71c766516f80
Georgii <gvk3404@mail.ru>
2022-05-27 12:42:54 +0700

    Edit test

--- test/board_test.c
+++ test/board_test.c
@@ -1,6 +1,6 @@
-#include "../src/board/functions.c"
-#include "../src/board/functions.h"
 #include "../thirdparty/ctest.h"
+#include "../src/board/functions.h"
+#include "../src/board/functions.c"
 #include <stdio.h>
 
 CTEST(read_txt, simple_read_file)
@@ -21,21 +21,22 @@ CTEST(read_txt, checkMoveCorrect)
     char positions[8];
     fgets(positions, 8, file);
     fclose(file);
-
+    
     int ANumber = 56;
-    const int res_error = 0;
+    const int res_error = 0; 
     int size = 9;
     char a[size][size];
     chess(a);
 
-    int startSecondIndex = 8 - ((positions[1] - ANumber) * (-1));
-
-    int finalSecondIndex = 8 - ((positions[4] - ANumber) * (-1));
+    int startSecondIndex = 8 - ((positions[1] - ANumber)*(-1));
+    
+    int finalSecondIndex = 8 - ((positions[4] - ANumber)*(-1));
 
     int temp = checkMove(8, positions, startSecondIndex, finalSecondIndex);
-
+    
     ASSERT_EQUAL(res_error, temp);
 }
+
 
 CTEST(read_txt, checkMoveUncorrect)
 {
@@ -43,15 +44,15 @@ CTEST(read_txt, checkMoveUncorrect)
     char positions[8];
     fgets(positions, 8, file);
     fclose(file);
-
+    
     int ANumber = 56;
-    const int res_error = 1;
+    const int res_error = 1; 
     int size = 9;
     char a[size][size];
     chess(a);
-    int startSecondIndex = 8 - ((positions[1] - ANumber) * (-1));
-    int finalSecondIndex = 8 - ((positions[4] - ANumber) * (-1));
+    int startSecondIndex = 8 - ((positions[1] - ANumber)*(-1));
+    int finalSecondIndex = 8 - ((positions[4] - ANumber)*(-1));
     int temp = checkMove(8, positions, startSecondIndex, finalSecondIndex);
-
+    
     ASSERT_EQUAL(res_error, temp);
 }

f29e5082195fade8e285adce795ef0c377002e3d
Georgii <gvk3404@mail.ru>
2022-05-27 12:55:04 +0700

    Edit error

--- test/board_test.c
+++ test/board_test.c
@@ -15,28 +15,6 @@ CTEST(read_txt, simple_read_file)
     ASSERT_STR(result, expected);
 }
 
-CTEST(read_txt, checkMoveCorrect)
-{
-    FILE* file = fopen("test.txt", "r");
-    char positions[8];
-    fgets(positions, 8, file);
-    fclose(file);
-    
-    int ANumber = 56;
-    const int res_error = 0; 
-    int size = 9;
-    char a[size][size];
-    chess(a);
-
-    int startSecondIndex = 8 - ((positions[1] - ANumber)*(-1));
-    
-    int finalSecondIndex = 8 - ((positions[4] - ANumber)*(-1));
-
-    int temp = checkMove(8, positions, startSecondIndex, finalSecondIndex);
-    
-    ASSERT_EQUAL(res_error, temp);
-}
-
 
 CTEST(read_txt, checkMoveUncorrect)
 {
